4f00cec21c2b54f6ec50a544fbda31f280d01515
Andry <andry.sheremetew@gmail.com>
2024-07-29 19:14:57 +0300

    77777777777

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,15 @@
 import './Navigation.css'
 
+const textFour = "nav 1";
+
 const Navigation = () => {
+
+  const textFive = "nav 2";
+
   return <div className='Navigation'>
     <ul className='nav'>
-      <li className='listNav'>nav 1</li>
-      <li className='listNav'>nav 2</li>
+      <li className='listNav'>{textFour}</li>
+      <li className='listNav'>{textFive}</li>
       <li className='listNav'>nav 3</li>
       <li className='listNav'>nav 4</li>
     </ul>
